f7c3a78fc6141c66fe99b0ed141cc365adf37011
Tjzeller12 <thomaszeller0@gmail.com>
2024-06-30 14:48:54 -0500

    config update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/PortfolioWebsite/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      'three': resolve(__dirname, 'node_modules/three/build/three.module.js'),
+    },
+  },
 });
