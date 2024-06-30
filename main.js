2db2f7b0dea459948c1d0d344da6ce72183f82c6
Tjzeller12 <thomaszeller0@gmail.com>
2024-06-30 14:11:25 -0500

    Three update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css';
-import * as THREE from 'https://unpkg.com/three@0.120.1/build/three.module.js';
+import * as THREE from 'three';
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 //import { add } from 'three/examples/jsm/libs/tween.module.js';
 
