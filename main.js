f7b40bd1fd95d25add53a0350156be9b5c780a85
Tjzeller12 <thomaszeller0@gmail.com>
2024-06-30 13:42:40 -0500

    remove orbit controls

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { add } from 'three/examples/jsm/libs/tween.module.js';
+//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+//import { add } from 'three/examples/jsm/libs/tween.module.js';
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
